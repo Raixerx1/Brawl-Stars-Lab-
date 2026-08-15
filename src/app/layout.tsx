c7a277584824a9b5687c949c685ad7c1791b1991
Raixerx1 <jps6953@gmail.com>
2026-08-15 21:10:24 +0200

    Load live review v0.18 styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./draft-overrides.css";
+import "./live-v18.css";
 import "./pwa.css";
 import AppShell from "@/components/AppShell";
 import PwaInstallPrompt from "@/components/PwaInstallPrompt";
