65afd5b27d74bebc2ffaeec8f8323cdfea28972e
Raixerx1 <jps6953@gmail.com>
2026-08-15 15:56:42 +0200

    Evitar contenido bajo la barra de estado en iPhone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     title: "Draft Lab",
-    statusBarStyle: "black-translucent",
+    statusBarStyle: "black",
   },
 };
 
